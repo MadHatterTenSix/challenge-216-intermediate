19b46298a0fb358fdcc8ac0c91080e451ccec6b5
MadHatterTenSix <beastwood2013@gmail.com>
2015-05-28 12:21:19 -0600

    Create TexasHoldEm.java

---
+++ TexasHoldEm.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author __MadHatter (alias used on https://www.reddit.com/r/dailyprogrammer)
+ */
+
+/* TexasHoldem.java */
+
+public final class TexasHoldEm {
+
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.start();
+  }
+
+}
